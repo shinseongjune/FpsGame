c3b06c5dfa445989948235646233830b3e1f76f4
sodaus145 <shinseongjune.dev@gmail.com>
2021-03-06 17:35:29 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+
+    CharacterController cc;
+
+    float gravity = -20f;
+
+    float yVelocity = 0;
+
+    public float jumpPower = 10f;
+
+    public bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        
+        dir = dir.normalized;
+
+        dir = Camera.main.transform.TransformDirection(dir);
+        
+        if (cc.collisionFlags == CollisionFlags.None)
+        {
+            isJumping = true;
+        }
+
+        if (isJumping && cc.collisionFlags == CollisionFlags.Below)
+        {
+            isJumping = false;
+            yVelocity = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+
+    }
+}
